497ea48fadfbc7218e203c5e4d6bf3b1f82568af
jukbot <vchukkrit@gmail.com>
2017-04-17 02:00:11 +0700

    app localization - language selector

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -2,9 +2,31 @@ module.exports = {
   staticFileGlobs: [
     '/index.html',
     '/manifest.json',
-    '/images/*',
     '/bower_components/webcomponentsjs/webcomponents-lite.min.js',
+    '/images/*'
   ],
   navigateFallback: '/index.html',
-  navigateFallbackWhitelist: [/^(?!\/__)/],
+  navigateFallbackWhitelist: [/^(?!.*\.html$|\/data\/).*/],
+  runtimeCaching: [
+    {
+      urlPattern: /\/data\/images\/.*/,
+      handler: 'cacheFirst',
+      options: {
+        cache: {
+          maxEntries: 200,
+          name: 'items-cache'
+        }
+      }
+    },
+    {
+      urlPattern: /\/data\/.*json/,
+      handler: 'fastest',
+      options: {
+        cache: {
+          maxEntries: 100,
+          name: 'data-cache'
+        }
+      }
+    }
+  ]
 };
