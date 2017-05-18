9d2f9de1f4029716e6ece020f906583b067b8896
jukbot <vchukkrit@gmail.com>
2017-05-18 18:22:24 +0700

    optimize build for production

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -1,35 +1,24 @@
+/**
+ * @license
+ * Copyright (c) 2016 The Polymer Project Authors. All rights reserved.
+ * This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+ * The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+ * The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+ * Code distributed by Google as part of the polymer project is also
+ * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+ */
+
+/* eslint-env node */
+
 module.exports = {
   staticFileGlobs: [
     '/index.html',
     '/manifest.json',
-    '/bower_components/webcomponentsjs/webcomponents-lite.min.js',
+    '/bower_components/webcomponentsjs/*',
     '/images/*',
     '/data/*',
     '/js/*',
     '/src/*',
   ],
   navigateFallback: '/index.html',
-  navigateFallbackWhitelist: [/^(?!.*\.html$|\/data\/).*/],
-  runtimeCaching: [
-    {
-      urlPattern: /\/images\//,
-      handler: 'cacheFirst',
-      options: {
-        cache: {
-          maxEntries: 100,
-          name: 'images-cache'
-        }
-      }
-    },
-    {
-      urlPattern: /\/data\/.*json/,
-      handler: 'fastest',
-      options: {
-        cache: {
-          maxEntries: 10,
-          name: 'localize-cache'
-        }
-      }
-    }
-  ]
 };
