69b978bcc94edf3435491817c8c898386d25b01a
jukbot <vchukkrit@gmail.com>
2017-05-29 19:28:35 +0700

    fix layout

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,17 @@
-var functions = require('firebase-functions');
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// })
+// Take the text parameter passed to this HTTP endpoint and insert it into the
+// Realtime Database under the path /messages/:pushId/original
+exports.addMessage = functions.https.onRequest((req, res) => {
+    // Grab the text parameter.
+    const original = req.query.text;
+    // Push it into the Realtime Database then send a response
+    admin.database().ref('/messages').push({
+        original: original
+    }).then(snapshot => {
+        // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+        res.redirect(303, snapshot.ref);
+    });
+});
