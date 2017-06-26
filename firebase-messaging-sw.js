af2c7587f59a88b7cfffae88abb204b228cd992f
jukbot <vchukkrit@gmail.com>
2017-06-26 14:24:18 +0700

    add eslint rules

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,13 +1,13 @@
 // Give the service worker access to Firebase Messaging.
 // Note that you can only use Firebase Messaging here, other Firebase libraries
 // are not available in the service worker.
-importScripts('/bower_components/firebase/firebase-app.js');
-importScripts('/bower_components/firebase/firebase-messaging.js');
+importScripts("/bower_components/firebase/firebase-app.js");
+importScripts("/bower_components/firebase/firebase-messaging.js");
 
 // Initialize the Firebase app in the service worker by passing in the
 // messagingSenderId.
 firebase.initializeApp({
-  'messagingSenderId': '253401775445'
+  messagingSenderId: "253401775445"
 });
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
