c005b0599e44227d9b6529d005b92507252b990c
jukbot <vchukkrit@gmail.com>
2017-09-20 03:27:14 +0700

    :hammer: Refactoring redux

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+"use strict";
+/* global __dirname module require*/
+/* eslint comma-dangle: ["error", "never"] */
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: "./src/view-app.html",
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "./build"),
+    publicPath: "build/"
+  },
+  resolve: {
+    modules: ["node_modules", "bower_components"],
+    descriptionFiles: ["package.json"]
+  },
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["env"],
+              plugins: ["syntax-dynamic-import"]
+            }
+          },
+          {
+            loader: "polymer-webpack-loader"
+          }
+        ]
+      },
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["env"]
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
