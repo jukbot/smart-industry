e3571dbcb234151f85823f60d8a043d30d0a19e0
jukbot <vchukkrit@gmail.com>
2017-10-13 14:55:41 +0700

    update bower

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,13 @@
-const functions = require('firebase-functions');
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
+const functions = require("firebase-functions");
+const Firestore = require("@google-cloud/firestore");
+const admin = require("firebase-admin");
+admin.initializeApp(functions.config().firebase);
+const firestore = new Firestore();
+// exports.helloFirestore = functions.https.onRequest((request, response) => {
+//   const helloFirestoreRef = firestore.doc(`test/hello`);
+//   var helloDoc = {
+//     msg: "Hello Firestore"
+//   };
+//   helloFirestoreRef.set(helloDoc);
+//   response.send("Data inserted");
 // });
