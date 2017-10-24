eea37bf50d321e5be9bdedf44941181f77c5e798
jukbot <vchukkrit@gmail.com>
2017-10-25 06:24:34 +0700

    migrate to preload script

---
+++ index.js
@@ -0,0 +1,20 @@
+// const prpl = require("prpl-server");
+// const express = require("express");
+
+// const app = express();
+
+// app.get("/api/launch", (req, res, next) => res.send("boom"));
+
+// app.get(
+//   "/*",
+//   prpl.makeHandler(".", {
+//     builds: [
+//       { name: "modern", browserCapabilities: ["es2015", "push"] },
+//       { name: "fallback" }
+//     ]
+//   })
+// );
+
+// app.listen(8080);
+
+// IN TESTING OF PRPL SERVER NODE
