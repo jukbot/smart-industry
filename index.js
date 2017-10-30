d1f9512277cccc8d7fe705a645aca92dbd5a1881
jukbot <vchukkrit@gmail.com>
2017-10-30 18:37:59 +0700

    fix add machine

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-// const prpl = require("prpl-server");
-// const express = require("express");
-
-// const app = express();
-
 // app.get("/api/launch", (req, res, next) => res.send("boom"));
 
 // app.get(
@@ -18,3 +13,26 @@
 // app.listen(8080);
 
 // IN TESTING OF PRPL SERVER NODE
+
+const critical = require("critical");
+const prpl = require("prpl-server");
+const express = require("express");
+const app = express();
+
+critical.generate({
+  // เป็นการบอก critical ว่าหลังจากแกะ critical CSS ออกมาแล้ว
+  // ให้นำก้อน CSS ที่ได้ไปแปะลง <style>...</style>
+  // ในส่วน head หรือที่เราเรียกกันว่า inline style
+  inline: true,
+  src: "index.html",
+  css: ["main.css"],
+  // ต้องการให้ผลลัพธ์ที่ได้อยู่ในไฟล์ไหน
+  dest: "index-critical.html",
+  // Viewport หรือขนาดของบราวเซอร์ที่ใช้
+  width: 1600,
+  height: 900,
+  // ลดขนาดไฟล์ด้วยการทำ minify
+  minify: true,
+  // อย่างที่กล่าวข้างต้น font-face ไม่ควรเป็น critical CSS
+  ignore: ["font-face"]
+});
