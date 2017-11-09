285b746beeab63cc19734d2d7add3917b551c701
jukbot <vchukkrit@gmail.com>
2017-11-10 04:54:40 +0700

    update prpl server

--- app.go
+++ app.go
@@ -1,30 +1,31 @@
 package app
 
 import (
-	"os"
 	"net/http"
-	"github.com/jukbot/smart-mes"
+	"os"
+
+	"github.com/captaincodeman/prpl-server-go"
 )
 
 func init() {
 	version := os.Getenv("STATIC_VERSION")
 	m, _ := prpl.New(
 		prpl.WithVersion(version),
-		prpl.WithRoot("./"),
-		prpl.WithConfigFile("./polymer.json"),
+		prpl.WithRoot("./build/default"),
+		prpl.WithConfigFile("./build/polymer.json"),
 		prpl.WithRoutes(prpl.Routes{
-        "/dashboard" : "src/view-dashboard.html",
-        "/setup": "src/view-setup.html",
-        "/plan": "src/view-plan.html",
-        "/track": "src/view-track.html",
-        "/settings": "src/view-settings.html",
-    	"/login": "src/view-login.html",
-		"/404": "src/view-404.html",
-		"/help":  "src/view-help.html",
-		"/term":  "src/view-term.html",
-		"/error":  "src/view-error.html",
-		"/offline":  "src/view-offline.html",
-		"/lazy-resources":  "src/lazy-resources.html"
+			"/dashboard":      "src/view-dashboard.html",
+			"/setup":          "src/view-setup.html",
+			"/plan":           "src/view-plan.html",
+			"/track":          "src/view-track.html",
+			"/settings":       "src/view-settings.html",
+			"/login":          "src/view-login.html",
+			"/404":            "src/view-404.html",
+			"/help":           "src/view-help.html",
+			"/term":           "src/view-term.html",
+			"/error":          "src/view-error.html",
+			"/offline":        "src/view-offline.html",
+			"/lazy-resources": "src/lazy-resources.html",
 		}),
 	)
 
