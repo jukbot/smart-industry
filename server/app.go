509cc49f864ff64ceb11bdbbd3a9a6a9cbcde78c
jukbot <vchukkrit@gmail.com>
2017-11-10 05:47:14 +0700

    fix app.go

--- server/app.go
+++ server/app.go
@@ -11,9 +11,23 @@ func init() {
 	version := os.Getenv("STATIC_VERSION")
 	m, _ := prpl.New(
 		prpl.WithVersion(version),
-		prpl.WithRoot("./"),
-		prpl.WithConfigFile("./polymer.json"),
+		prpl.WithRoot("./build"),
+		prpl.WithConfigFile("./build/polymer.json"),
+		prpl.WithRoutes(prpl.Routes{
+			"/dashboard":      "src/view-dashboard.html",
+			"/setup":          "src/view-setup.html",
+			"/plan":           "src/view-plan.html",
+			"/track":          "src/view-track.html",
+			"/settings":       "src/view-settings.html",
+			"/login":          "src/view-login.html",
+			"/404":            "src/view-404.html",
+			"/help":           "src/view-help.html",
+			"/term":           "src/view-term.html",
+			"/error":          "src/view-error.html",
+			"/offline":        "src/view-offline.html",
+			"/lazy-resources": "src/lazy-resources.html",
+		}),
 	)
 
-	http.ListenAndServe(":8080", m)
+	http.Handle("/", m)
 }
