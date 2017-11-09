8571b35985617b04d3828bb9e7dc95eab38d34b7
jukbot <vchukkrit@gmail.com>
2017-11-09 20:05:21 +0700

    add prpl server go for production

--- index.js
+++ index.js
@@ -1,44 +1,26 @@
+const prpl = require("prpl-server");
 const express = require("express");
+
 const app = express();
-// const critical = require("critical");
-// const prpl = require("prpl-server");
 
-// Serve static files from the main build directory
-app.use(express.static(__dirname + "/build/default"));
+app.get("/api/launch", (req, res, next) => res.send("boom"));
 
-app.get("*", function(req, res) {
-  res.sendFile("index.html", { root: "." });
+app.get(
+  "/*",
+  prpl.makeHandler(".", {
+    builds: [
+      { name: "modern", browserCapabilities: ["es2015", "push"] },
+      { name: "fallback" }
+    ]
+  })
+);
+
+app.get("/images/*", (req, res, next) => {
+  res.setHeader("Cache-Control", "public, max-age=31536000");
+  next();
 });
 
-// Tell the app to listen for requests on port 3000
-app.listen(3000, function() {
-  console.info("Example app listening on port 3000!");
+// Tell the app to listen for requests on port 8080
+app.listen(8080, function() {
+  console.info("Example app listening on port 8080!");
 });
-
-// app.get(
-//   "/*",
-//   prpl.makeHandler(".", {
-//     builds: [
-//       { name: "modern", browserCapabilities: ["es2015", "push"] },
-//       { name: "fallback" }
-//     ]
-//   })
-// );
-
-// critical.generate({
-//   // เป็นการบอก critical ว่าหลังจากแกะ critical CSS ออกมาแล้ว
-//   // ให้นำก้อน CSS ที่ได้ไปแปะลง <style>...</style>
-//   // ในส่วน head หรือที่เราเรียกกันว่า inline style
-//   inline: true,
-//   src: "index.html",
-//   css: ["main.css"],
-//   // ต้องการให้ผลลัพธ์ที่ได้อยู่ในไฟล์ไหน
-//   dest: "index-critical.html",
-//   // Viewport หรือขนาดของบราวเซอร์ที่ใช้
-//   width: 1600,
-//   height: 900,
-//   // ลดขนาดไฟล์ด้วยการทำ minify
-//   minify: true,
-//   // อย่างที่กล่าวข้างต้น font-face ไม่ควรเป็น critical CSS
-//   ignore: ["font-face"]
-// });
