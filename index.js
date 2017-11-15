e50604b63779254bbbeceb7270596c9fefea9932
jukbot <vchukkrit@gmail.com>
2017-11-16 06:51:32 +0700

    fix route

--- index.js
+++ index.js
@@ -1,36 +1,47 @@
-import { prpl } from 'prpl-server'
-import { express } from 'express'
+import { prpl } from "prpl-server";
+import { express } from "express";
 
 const app = express();
-const rootDir = '/';
+const rootDir = "/";
 
-app.get('/api/launch', (req, res, next) => res.send('boom'));
+// This serves static files from the specified directory
+app.use(express.static(__dirname + "/build"));
+
+app.get("/api/launch", (req, res, next) => res.send("boom"));
 
 app.get(
-  '/*',
-  prpl.makeHandler('.', {
-    entrypoint: 'index.html',
+  "/*",
+  prpl.makeHandler(".", {
     builds: [
-      {name: 'modern', browserCapabilities: ['es2015', 'push'], basePath: true},
+      {
+        name: "modern",
+        browserCapabilities: ["es2015", "push"],
+        basePath: true
+      },
+      { name: "fallback" }
     ],
     forwardErrors: true
   })
 );
 
-app.get('/images/*', (req, res, next) => {
-  res.setHeader('Cache-Control', 'public, max-age=31536000');
+app.get("/images/*", (req, res, next) => {
+  res.setHeader("Cache-Control", "public, max-age=31536000");
   next();
 });
 
 app.use((err, req, res, next) => {
   if (err.status === 404) {
-    res.status(404).sendFile('src/view-404.html', {root: rootDir});
+    res.status(404).sendFile("src/view-404.html", { root: rootDir });
   } else {
     next();
   }
 });
 
 // Tell the app to listen for requests on port 8080
-app.listen(8080, function() {
-  Console.log('Application is listening on port 8080!');
+const server = app.listen(8080, () => {
+  const host = server.address().address;
+  const port = server.address().port;
+
+  console.log("App listening at http://%s:%s", host, port);
+  c4swd;
 });
