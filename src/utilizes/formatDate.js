7d80c2156416a264bb42619d514687d906b87ef8
Chukkrit Visitsaktavorn <vchukkrit@gmail.com>
2018-02-06 02:39:22 +0700

    refactoring project

--- src/utilizes/formatDate.js
+++ src/utilizes/formatDate.js
@@ -1,32 +1,35 @@
-'use strict'
+"use strict";
 
-  function isLeapYear() {  
-    let year = new Date().getFullYear()
-    return year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0)
-  }
+function isLeapYear() {
+  let year = new Date().getFullYear();
+  return year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0);
+}
 
- function daysOfTheYear() {
-    const now = new Date();
-    let start = new Date(now.getFullYear(), 0, 0);
-    let diff = (now - start) + ((start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000);
-    let oneDay = 1000 * 60 * 60 * 24;
-    let day = Math.floor(diff / oneDay);
-    return day;
- }
-
- function dateFromDays(day){
+function dateFromDays(day) {
   let timestamp = new Date(new Date(new Date().getFullYear(), 0)).setDate(day); // initialize a date in `year-01-01`
   let date = new Date(timestamp);
-  let dd = date.getDate()
-  let mm = date.getMonth() + 1
-  let yyyy = date.getFullYear()
+  let dd = date.getDate();
+  let mm = date.getMonth() + 1;
+  let yyyy = date.getFullYear();
   if (dd < 10) {
-      dd = '0' + dd
+    dd = "0" + dd;
   }
   if (mm < 10) {
-      mm = '0' + mm
+    mm = "0" + mm;
   }
-  return dd + '/' + mm + '/' + yyyy
+  return dd + "/" + mm + "/" + yyyy;
+}
+
+function daysOfTheYear() {
+  const now = new Date();
+  let start = new Date(now.getFullYear(), 0, 0);
+  let diff =
+    now -
+    start +
+    (start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000;
+  let oneDay = 1000 * 60 * 60 * 24;
+  let day = Math.floor(diff / oneDay);
+  return day;
 }
 
 //  function datesInTheYear() {
@@ -44,9 +47,9 @@
 //     }
 //     return dates;
 //   });
-  
+
 //   // Jan start at 0
-//   let dates = getDates(new Date(new Date().getFullYear(),0,1), new Date(new Date().getFullYear(),11,31));                                                                                                         
+//   let dates = getDates(new Date(new Date().getFullYear(),0,1), new Date(new Date().getFullYear(),11,31));
 //   dates.forEach(function(date) {
 //     console.log(date);
 //   });
