ad93dd858ccef53b454d885cff0ebfd95ead50af
jukbot <chukkrit.juk@gmail.com>
2018-06-14 20:18:10 +0700

    update firebase functions

--- functions/index.js
+++ functions/index.js
@@ -1,17 +1,16 @@
-const functions = require("firebase-functions");
-const prpl = require("prpl-server");
-const express = require("express");
-const rendertron = require("rendertron-middleware");
+const express = require('express');
+const prpl = require('prpl-server');
+const functions = require('firebase-functions');
+const rendertron = require('rendertron-middleware');
+const config = require('./build/polymer.json');
 
 const app = express();
 
-app.use(
-  rendertron.makeMiddleware({
-    proxyUrl: "https://render-tron.appspot.com/render",
-    injectShadyDom: true
-  })
-);
+app.use(rendertron.makeMiddleware({
+  proxyUrl: 'https://render-tron.appspot.com/render',
+  injectShadyDom: true,
+}));
 
-app.get("/*", prpl.makeHandler("./build", require("./build/polymer.json")));
+app.get('*', prpl.makeHandler('./build', config));
 
 exports.app = functions.https.onRequest(app);
